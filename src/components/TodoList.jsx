8ee0adc67f20efcf3541ed5d4d417ab1ebced8da
khushbu <pujakumari29122001@gmail.com>
2025-11-10 00:14:50 +0530

    Added TodoList and TodoItem

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,7 +5,7 @@ const TodoList = ({ todos, onDelete, onToggle, onEdit }) => {
   return (
     <section className="todo-section">
       {todos.length === 0 ? (
-        <p className="no-task">No tasks yet — start adding one above!</p>
+        <p className="no-task">No tasks yet start adding one above !</p>
       ) : (
         <ul className="todo-list">
           {todos.map((todo) => (
